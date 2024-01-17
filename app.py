951c7d5f0593cbb5000602deaafaeb2670cf69c1
Samuel Vieira <74198324+samuel-vieira-dev@users.noreply.github.com>
2024-01-17 13:50:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-ACTIVECAMPAIGN_BASE_URL = "https://febracis65440.api-us1.com/"
+ACTIVECAMPAIGN_BASE_URL = "https://febracis65440.api-us1.com/api/3"
 API_TOKEN = "c383bbd6bec55c9932f1f94ecb3580c9f1bba25d8c56e2ed2001501af161a81a6a3cb2ed"
 
 @app.route('/save-lead', methods=['POST'])
